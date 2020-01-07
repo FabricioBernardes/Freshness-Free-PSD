2dfbade60858331e3eb424f54b343a13260542e1
FabricioBernardes <FabricioBernardes@users.noreply.github.com>
2020-01-07 10:00:57 -0300

    .

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,38 @@
-var x=document.querySelectorAll("#slider > .hide-slide");
-var y=document.querySelectorAll("#dots-container > span");
+var selectslides=document.querySelectorAll("#slider > .hide-slide");
+var selectdots=document.querySelectorAll("#dots-container > span");
+var timer=setInterval (slideLoop, 3000);
+var y=0;
 
-
-x[0].classList.add("show-slide");
-y[0].classList.add("dot-click");
-
-
-
-function slideselect (n) {
-    x[0].classList.remove("show-slide");
-    x[1].classList.remove("show-slide");
-    x[2].classList.remove("show-slide");
-    x[3].classList.remove("show-slide");
-    x[4].classList.remove("show-slide");
-    x[n].classList.add("show-slide");
-
-    y[0].classList.remove("dot-click");
-    y[1].classList.remove("dot-click");
-    y[2].classList.remove("dot-click");
-    y[3].classList.remove("dot-click");
-    y[4].classList.remove("dot-click");
-    y[n].classList.add("dot-click");
+function slideLoop (){
+    y++;
+    if (y>4){
+        y=0;
+    }
+    changeSlide (y);
 }
+
+function clickDot (n) {
+    y=n;
+    changeSlide (y);
+    clearInterval (timer);
+    timer=setInterval (slideLoop, 3000);
+}
+
+function changeSlide (y) {
+    selectslides[0].classList.remove("show-slide");
+    selectslides[1].classList.remove("show-slide");
+    selectslides[2].classList.remove("show-slide");
+    selectslides[3].classList.remove("show-slide");
+    selectslides[4].classList.remove("show-slide");
+    selectslides[y].classList.add("show-slide");
+
+    selectdots[0].classList.remove("dot-click");
+    selectdots[1].classList.remove("dot-click");
+    selectdots[2].classList.remove("dot-click");
+    selectdots[3].classList.remove("dot-click");
+    selectdots[4].classList.remove("dot-click");
+    selectdots[y].classList.add("dot-click");
+}
+
+selectslides[0].classList.add("show-slide");
+selectdots[0].classList.add("dot-click");
